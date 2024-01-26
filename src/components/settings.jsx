16f9bfc884452027cb74dbc8e3ce17f318fdcefe
Jhontabo <jhonse.tajumbina@gmail.com>
2024-01-26 10:08:18 -0500

    commit antes del merge

---
+++ src/components/settings.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Settings = () => (
+  <div className='contenido-pagina'>
+    <h2>Ajustes</h2>
+    {/* Contenido de la página de inicio */}
+  </div>
+);
+
+export default Settings;

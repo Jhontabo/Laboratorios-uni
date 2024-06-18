be5f12e2313f95019d8ed5a6891abde57300f683
Jhontabo <jhonse.tajumbina@gmail.com>
2024-06-18 11:35:45 -0500

    correcion de colores en dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,13 +24,12 @@ const Header = () => {
     }
   };
 
-
   return (
-    <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
+    <header className="h-[7vh] md:h-[10vh] border-b border-gray-300 p-8 flex items-center justify-end">
       <nav className="flex items-center gap-2">
         <Menu
           menuButton={
-            <MenuButton className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+            <MenuButton className="relative hover:bg-gray-200 p-2 rounded-lg transition-colors">
               <RiNotification3Line />
               <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold">
                 2
@@ -40,17 +39,17 @@ const Header = () => {
           align="end"
           arrow
           transition
-          arrowClassName="bg-secondary-100"
-          menuClassName="bg-secondary-100 p-4"
+          arrowClassName="bg-gray-200"
+          menuClassName="bg-white p-4 shadow-md"
         >
-          <h1 className="text-gray-300 text-center font-medium">
+          <h1 className="text-gray-700 text-center font-medium">
             Notificaciones (2)
           </h1>
-          <hr className="my-6 border-gray-500" />
+          <hr className="my-6 border-gray-300" />
           <MenuItem className="p-0 hover:bg-transparent">
             <Link
               to="/"
-              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-gray-200 transition-colors rounded-lg"
             >
               <img
                 src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
@@ -61,7 +60,7 @@ const Header = () => {
                   <span>Jorge Luis Trejo</span>{" "}
                   <span className="text-[8px]">21/10/2022</span>
                 </div>
-                <p className="text-gray-500 text-xs">
+                <p className="text-gray-600 text-xs">
                   Lorem ipsum dolor sit amet...
                 </p>
               </div>
@@ -70,15 +69,15 @@ const Header = () => {
           <MenuItem className="p-0 hover:bg-transparent">
             <Link
               to="/"
-              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-gray-200 transition-colors rounded-lg"
             >
-              <RiThumbUpLine className="p-2 bg-blue-200 text-blue-700 box-content rounded-full" />
+              <RiThumbUpLine className="p-2 bg-blue-500 text-white box-content rounded-full" />
               <div className="text-sm flex flex-col">
                 <div className="flex items-center justify-between gap-4">
                   <span>Nuevo like</span>{" "}
                   <span className="text-[8px]">21/10/2022</span>
                 </div>
-                <p className="text-gray-500 text-xs">
+                <p className="text-gray-600 text-xs">
                   A Jorge Trejo le gusta tu pub...
                 </p>
               </div>
@@ -87,25 +86,25 @@ const Header = () => {
           <MenuItem className="p-0 hover:bg-transparent">
             <Link
               to="/"
-              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-gray-200 transition-colors rounded-lg"
             >
-              <RiChat3Line className="p-2 bg-yellow-200 text-yellow-700 box-content rounded-full" />
+              <RiChat3Line className="p-2 bg-yellow-500 text-white box-content rounded-full" />
               <div className="text-sm flex flex-col">
                 <div className="flex items-center justify-between gap-4">
                   <span>Nuevo comentario</span>{" "}
                   <span className="text-[8px]">21/10/2022</span>
                 </div>
-                <p className="text-gray-500 text-xs">
+                <p className="text-gray-600 text-xs">
                   Jorge Trejo ha comentado tu...
                 </p>
               </div>
             </Link>
           </MenuItem>
-          <hr className="my-6 border-gray-500" />
+          <hr className="my-6 border-gray-300" />
           <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
             <Link
               to="/"
-              className="text-gray-400 text-sm hover:text-white transition-colors"
+              className="text-gray-600 text-sm hover:text-gray-800 transition-colors"
             >
               Todas las notificaciones
             </Link>
@@ -113,7 +112,7 @@ const Header = () => {
         </Menu>
         <Menu
           menuButton={
-            <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+            <MenuButton className="flex items-center gap-x-2 hover:bg-gray-200 p-2 rounded-lg transition-colors">
               <img
                 src={user.photoURL}
                 className="w-6 h-6 object-cover rounded-full"
@@ -124,43 +123,42 @@ const Header = () => {
           }
           align="end"
           arrow
-          arrowClassName="bg-secondary-100"
+          arrowClassName="bg-gray-200"
           transition
-          menuClassName="bg-secondary-100 p-4"
+          menuClassName="bg-white p-4 shadow-md"
         >
           <MenuItem className="p-0 hover:bg-transparent">
             <Link
               to="/perfil"
-              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+              className="rounded-lg transition-colors text-gray-700 hover:bg-gray-200 flex items-center gap-x-4 py-2 px-6 flex-1"
             >
               <img
                 src={user.photoURL} className="w-8 h-8 object-cover rounded-full"
               />
               <div className="flex flex-col text-sm">
                 <span className="text-sm">{user.displayName}</span>
-                <span className="text-xs text-gray-500">{user.email}</span>
+                <span className="text-xs text-gray-600">{user.email}</span>
               </div>
             </Link>
           </MenuItem>
-          <hr className="my-4 border-gray-500" />
+          <hr className="my-4 border-gray-300" />
 
           <MenuItem className="p-0 hover:bg-transparent">
-
             <Link
               to="/configuracion"
-              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+              className="rounded-lg transition-colors text-gray-700 hover:bg-gray-200 flex items-center gap-x-4 py-2 px-6 flex-1"
             >
               <RiSettings3Line /> Configuración
             </Link>
           </MenuItem>
 
-
           <MenuItem className="p-0 hover:bg-transparent">
-            <button onClick={cerrarSesión} className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            <button
+              onClick={cerrarSesión}
+              className="rounded-lg transition-colors text-gray-700 hover:bg-gray-200 flex items-center gap-x-4 py-2 px-6 flex-1"
             >
               <RiLogoutCircleRLine /> Cerrar sesión
             </button>
-
           </MenuItem>
         </Menu>
       </nav>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -29,11 +29,11 @@ const Sidebar = () => {
     return (
         <>
             <div
-                className={`xl:h-[100vh] overflow-y-scroll fixed xl:static w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-gray-700 p-4 flex flex-col justify-between z-50 ${showMenu ? "left-0" : "-left-full"
+                className={`xl:h-[100vh] overflow-y-scroll fixed xl:static w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-gray-800 p-4 flex flex-col justify-between z-50 ${showMenu ? "left-0" : "-left-full"
                     } transition-all`}
             >
                 <div>
-                    <h1 className="text-center text-2xl font-bold text-black mb-10">
+                    <h1 className="text-center text-2xl font-bold text-white mb-10">
                         Dashboard<span className="text-primary text-4xl">.</span>
                     </h1>
 
@@ -41,22 +41,22 @@ const Sidebar = () => {
                         <li>
                             <Link
                                 to="/"
-                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-blue-500 transition-colors"
+                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors text-white"
                             >
-                                <RiBarChart2Line className="text-black" /> Analíticas
+                                <RiBarChart2Line className="text-white" /> Analíticas
                             </Link>
                         </li>
                         <li>
                             <button
                                 onClick={() => setShowSubmenu(!showSubmenu)}
-                                className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-blue-500 transition-colors"
+                                className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors text-white"
                             >
                                 <span className="flex items-center gap-4">
-                                    <RiEarthLine className="text-black" /> Redes sociales
+                                    <RiEarthLine className="text-white" /> Redes sociales
                                 </span>
                                 <RiArrowRightSLine
                                     className={`mt-1 ${showSubmenu && "rotate-90"
-                                        } transition-all`}
+                                        } transition-all text-white`}
                                 />
                             </button>
                             <ul
@@ -66,7 +66,7 @@ const Sidebar = () => {
                                 <li>
                                     <Link
                                         to="/"
-                                        className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white transition-colors"
+                                        className="py-2 px-4 border-l border-gray-600 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800 hover:text-white transition-colors text-gray-400"
                                     >
                                         Post red social
                                     </Link>
@@ -74,7 +74,7 @@ const Sidebar = () => {
                                 <li>
                                     <Link
                                         to="/"
-                                        className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white transition-colors"
+                                        className="py-2 px-4 border-l border-gray-600 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-600 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800 hover:text-white transition-colors text-gray-400"
                                     >
                                         Estadisticas
                                     </Link>
@@ -82,7 +82,7 @@ const Sidebar = () => {
                                 <li>
                                     <Link
                                         to="/"
-                                        className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white transition-colors"
+                                        className="py-2 px-4 border-l border-gray-600 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-600 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800 hover:text-white transition-colors text-gray-400"
                                     >
                                         Perfiles
                                     </Link>
@@ -92,18 +92,18 @@ const Sidebar = () => {
                         <li>
                             <Link
                                 to="/chat"
-                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-blue-500 transition-colors"
+                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors text-white"
                             >
-                                <RiCustomerService2Line className="text-black" /> Soporte
+                                <RiCustomerService2Line className="text-white" /> Soporte
                                 técnico
                             </Link>
                         </li>
                         <li>
                             <Link
                                 to="/calendario"
-                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-blue-500 transition-colors"
+                                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors text-white"
                             >
-                                <RiCalendarTodoLine className="text-black" /> Calendario
+                                <RiCalendarTodoLine className="text-white" /> Calendario
                             </Link>
                         </li>
                     </ul>
@@ -111,7 +111,7 @@ const Sidebar = () => {
                 </div>
 
                 <button
-                    onClick={cerrarSesión} className="flex items-center gap-4 py-2 px-4 rounded-lg  transition-colors  hover:bg-blue-500"
+                    onClick={cerrarSesión} className="flex items-center gap-4 py-2 px-4 rounded-lg transition-colors hover:bg-gray-700 text-white"
                 >
                     <RiLogoutCircleRLine className="text-red-500 " /> Cerrar sesión
                 </button>
@@ -119,12 +119,10 @@ const Sidebar = () => {
             </div>
 
             <button
-
                 onClick={() => setShowMenu(!showMenu)}
-                className="xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50 hover:bg-blue-500"
+                className="xl:hidden fixed bottom-4 right-4 bg-gray-800 text-white p-3 rounded-full z-50 hover:bg-gray-700"
             >
                 {showMenu ? <RiCloseLine /> : <RiMenu3Line />}
-
             </button>
         </>
     );

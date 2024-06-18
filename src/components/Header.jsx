43f36d95618ef7532f47e6d1c699713c451e1308
Jhontabo <jhonse.tajumbina@gmail.com>
2024-06-17 20:12:53 -0500

    cambios menores en estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,17 @@ import { Link } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Header = () => {
-  const { user, googleSignIn } = UserAuth();
+  const { user, logOut } = UserAuth();
+
+  const cerrarSesión = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
       <nav className="flex items-center gap-2">
@@ -133,7 +143,9 @@ const Header = () => {
             </Link>
           </MenuItem>
           <hr className="my-4 border-gray-500" />
+
           <MenuItem className="p-0 hover:bg-transparent">
+
             <Link
               to="/configuracion"
               className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
@@ -141,13 +153,14 @@ const Header = () => {
               <RiSettings3Line /> Configuración
             </Link>
           </MenuItem>
+
+
           <MenuItem className="p-0 hover:bg-transparent">
-            <Link
-              to="/cerrar-sesion"
-              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            <button onClick={cerrarSesión} className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
             >
               <RiLogoutCircleRLine /> Cerrar sesión
-            </Link>
+            </button>
+
           </MenuItem>
         </Menu>
       </nav>

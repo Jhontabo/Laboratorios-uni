92ebd7e5fd7ced5392716c44aeb419ab859c144d
Jhontabo <jhonse.tajumbina@gmail.com>
2024-06-23 11:57:48 -0500

    backend y frontend funcionando

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api', usuarioRoutes);
+app.use('/', usuarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);

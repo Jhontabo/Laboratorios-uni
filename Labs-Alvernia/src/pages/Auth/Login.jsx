d74ee34b7703633d05fa573b547bd4600471ad04
Jhontabo <jhonse.tajumbina@gmail.com>
2024-06-28 14:39:29 -0500

    Cambios en rama Develop

--- Labs-Alvernia/src/pages/Auth/Login.jsx
+++ Labs-Alvernia/src/pages/Auth/Login.jsx
@@ -33,15 +33,21 @@ const Login = () => {
     }, [user])
 
 
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     // Es la funcion que permite enviar los datos del formulario
     const handleSubmit = (e) => {
         e.preventDefault(); //Esto hace que la pagina se recargue cuando demos click en el boton
 
-        const email = e.target.email.value; //Captura el valor del campo del formulario
-        const password = e.target.password.value;
         //Imprime en consola el valor de los datos capturados
-        console.log('Email:', email);
-        console.log('Password:', password);
+        console.log({
+            email: email,
+            password: password
+
+        });
+
     };
 
 
@@ -51,24 +57,37 @@ const Login = () => {
         <div className={`flex justify-center items-center min-h-screen ${isDarkMode ? 'bg-gray-800' : 'bg-gray-100'}`}>
             <div className={`relative w-[470px] h-[520px] p-4 border ${isDarkMode ? 'bg-gray-700 border-gray-600' : 'bg-white border-gray-300'} rounded-lg`}>
                 <div className="flex flex-col text-center my-5">
-                    <header className={`font-poppins text-2xl mb-1 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Hola, bienvenido a CAMPUS VIRTUAL UMARIANA</header>
+                    <header className={`font-poppins text-2xl mb-1 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Sistema laboratorios Universidad Mariana</header>
                 </div>
+
+
                 <form className="relative w-full" onSubmit={handleSubmit}>
                     <div className="relative w-full mb-6">
-                        <input type="text" id="email" className={`w-full h-12 pl-4 mb-6 border ${isDarkMode ? 'bg-gray-600 border-gray-500 text-white' : 'bg-gray-100 border-gray-300 text-gray-800'} rounded-md outline-none focus:border-purple-600`} autoComplete="off" required />
+
+                        <input onChange={(event) => { setEmail(event.target.value) }} type="text" id="email" className={`w-full h-12 pl-4 mb-6 border ${isDarkMode ? 'bg-gray-600 border-gray-500 text-white' : 'bg-gray-100 border-gray-300 text-gray-800'} rounded-md outline-none focus:border-purple-600`} autoComplete="off" required />
+
                         <label htmlFor="email" className={`absolute top-3 left-4 transition-transform duration-150 transform-gpu scale-75 -translate-y-6 px-1 ${isDarkMode ? 'text-gray-300 bg-gray-700' : 'text-gray-600 bg-gray-100'}`}>Correo</label>
                     </div>
+
                     <div className="relative w-full mb-6">
-                        <input type="password" id="password" className={`w-full h-12 pl-4 mb-2 border ${isDarkMode ? 'bg-gray-600 border-gray-500 text-white' : 'bg-gray-100 border-gray-300 text-gray-800'} rounded-md outline-none focus:border-purple-600`} autoComplete="off" required />
+                        <input onChange={(event) => { setPassword(event.target.value) }} type="password" id="password" className={`w-full h-12 pl-4 mb-2 border ${isDarkMode ? 'bg-gray-600 border-gray-500 text-white' : 'bg-gray-100 border-gray-300 text-gray-800'} rounded-md outline-none focus:border-purple-600`} autoComplete="off" required />
+
                         <label htmlFor="password" className={`absolute top-3 left-4 transition-transform duration-150 transform-gpu scale-75 -translate-y-6 px-1 ${isDarkMode ? 'text-gray-300 bg-gray-700' : 'text-gray-600 bg-gray-100'}`}>Contraseña</label>
                     </div>
+
                     <div className="flex justify-between mb-6">
                         {/* Aquí podrías agregar más contenido */}
                     </div>
+
+
                     <div className="relative w-full mb-6">
                         <input type="submit" className={`font-poppins text-white text-lg ${isDarkMode ? 'bg-purple-500' : 'bg-purple-500'} border-none rounded-md cursor-pointer py-2 w-full`} value="Iniciar sesión" />
                     </div>
+
                 </form>
+
+
+
                 <div className="relative w-full my-8">
                     <hr className={`border-t ${isDarkMode ? 'border-gray-600' : 'border-gray-300'}`} />
                     <p className={`absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-2 ${isDarkMode ? 'bg-gray-700 text-gray-300' : 'bg-gray-100 text-gray-600'}`}>O</p>
--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,11 @@ import express from 'express';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/api', usuarioRoutes);
+app.use('/', usuarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);

065903a4abd60f9ca390b6dd7c315787f41387db
joburbanop <joburbanop589@gmail.com>
2024-10-12 18:56:34 -0500

    recurso laboratorio creado

---
+++ app/Filament/Resources/LaboratorioResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LaboratorioResource\Pages;
+use App\Filament\Resources\LaboratorioResource\RelationManagers;
+use App\Models\Laboratorio;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LaboratorioResource extends Resource
+{
+    protected static ?string $model = Laboratorio::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLaboratorios::route('/'),
+            'create' => Pages\CreateLaboratorio::route('/create'),
+            'edit' => Pages\EditLaboratorio::route('/{record}/edit'),
+        ];
+    }
+}

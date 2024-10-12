3c7831b95cbfb35a99c0c3f8e30828b740744255
joburbanop <joburbanop589@gmail.com>
2024-10-12 18:27:24 -0500

    se ajusto la migracion de la tabla productos para llevar un control mas detallado

--- database/migrations/2024_10_10_222273_create_productos_table.php
+++ database/migrations/2024_10_10_222273_create_productos_table.php
@@ -7,22 +7,29 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up()
-{
-    Schema::create('productos', function (Blueprint $table) {
-        $table->id('id_productos');
-        $table->string('nombre')->nullable();
-        $table->text('descripcion')->nullable();
-        $table->integer('cantidad_disponible')->nullable();
-        $table->foreignId('id_laboratorio')->constrained('laboratorio','id_laboratorio')->onDelete('cascade')->onUpdate('cascade');
-        $table->foreignId('id_categorias')->constrained('categorias','id_categorias')->onDelete('cascade')->onUpdate('cascade');
-        $table->string('Estado');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id('id_productos');
+            $table->string('nombre')->nullable();
+            $table->text('descripcion')->nullable();
+            $table->integer('cantidad_disponible')->nullable();
+            $table->foreignId('id_laboratorio')
+                  ->constrained('laboratorio','id_laboratorio')
+                  ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('id_categorias')
+                  ->constrained('categorias','id_categorias')
+                  ->onDelete('cascade')->onUpdate('cascade');
+            $table->string('numero_serie')->nullable(); // Nuevo campo
+            $table->date('fecha_adicion')->nullable(); // Fecha de adición del producto
+            $table->decimal('costo_unitario', 8, 2)->nullable(); // Costo unitario del equipo o suministro
+            $table->string('ubicacion')->nullable(); // Ubicación del equipo o suministro
+            $table->enum('estado', ['nuevo', 'usado', 'dañado'])->default('nuevo'); // Estado del equipo o suministro
+            $table->timestamps();
+        });
+    }
 
-public function down()
-{
-    Schema::dropIfExists('productos');
-}
-
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
 };

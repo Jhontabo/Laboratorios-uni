8d18ff0ff93e54cab10f8124944a7cd03477e4b2
Jhontabo <jhonse.tajumbina@gmail.com>
2024-10-24 11:24:51 -0500

    Guardar cambios antes de cambiar de rama

---
+++ database/migrations/2024_10_10_221038_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correo_electronico')->unique();
+            $table->string('telefono')->nullable();
+            $table->string('Direccion');
+            $table->string('name')->virtualAs('CONCAT(nombre, " ", apellido)'); // Nuevo campo concatenado
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};

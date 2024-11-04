902b95ac77212d28afbaa8c7b38c4bab366260db
joburbanop <joburbanop589@gmail.com>
2024-11-04 09:37:59 -0500

    calendario listo en reservas

--- app/Filament/Resources/ReservaResource.php
+++ app/Filament/Resources/ReservaResource.php
@@ -14,7 +14,7 @@ class ReservaResource extends Resource
 {
     protected static ?string $model = Reserva::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-arrow-path';
 
     public static function getPages(): array
     {

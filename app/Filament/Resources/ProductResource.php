3642238ccad8440bc7b855adafce87de093f490e
Jhontabo <jhonse.tajumbina@gmail.com>
2025-05-27 09:46:03 -0500

    Cambio de nombre del panel inventario

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -78,7 +78,7 @@ class ProductResource extends Resource
     protected static ?string $model = Product::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-cube';
-    protected static ?string $navigationLabel = 'Productos / Inventario';
+    protected static ?string $navigationLabel = 'Inventario';
     protected static ?string $navigationGroup = 'Inventario';
     protected static ?string $modelLabel = 'Producto';
     protected static ?string $pluralLabel = 'Productos';

8a7de97c59afbafb662a00a9396cd6c6775868f5
Jhontabo <jhonse.tajumbina@gmail.com>
2025-11-02 22:46:34 -0500

    fix:vite.config.js fixed

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
-                refreshPaths,
-                'app/Livewire/**',
-            ]
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
         }),
+        react(),
     ],
 });
